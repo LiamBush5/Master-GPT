03d5eedd61061662afc4f19d9661d85a813ee6d4
LiamBush5 <105826006+LiamBush5@users.noreply.github.com>
2024-01-25 10:56:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+# Title of the directory page
+st.title('Directory Page')
+
+# Display links
+st.markdown('[Link 1](https://www.example1.com)')
+st.markdown('[Link 2](https://www.example2.com)')
+st.markdown('[Link 3](https://www.example3.com)')
