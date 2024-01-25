b41719be0dc29008593c641c82199fb7ea69bd1f
LiamBush5 <105826006+LiamBush5@users.noreply.github.com>
2024-01-25 11:10:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import streamlit as st
 # Title of the directory page
 st.title('Directory Page')
 
-# Display links
-st.markdown('[Link 1](https://www.example1.com)')
-st.markdown('[Link 2](https://www.example2.com)')
-st.markdown('[Link 3](https://www.example3.com)')
+# Subheading and link for Val GPT
+st.subheader('Val GPT')
+st.markdown('https://valiant-gpt-v2-khsd23hqrudqd67xagqtng.streamlit.app/')
+
+# Subheading and link for Policy GPT
+st.subheader('Policy GPT')
+st.markdown('https://valiant-policy-gpt-v2-khsd23hqrudqd67xagqtng.streamlit.app/')
+
+# Subheading and link for Summarize GPT
+st.subheader('Summarize GPT')
+st.markdown('https://rcl-summarize-ca7vsukofzajo4w8etgb9b.streamlit.app/')
